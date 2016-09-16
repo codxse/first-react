e3acd4bc9119d926d61f104f73b33d732f221dc8
Nadiar AS <codxse@gmail.com>
2016-09-16 15:38:29 +0700

    HTML on JavaScript called JSX stand for JavaScript XML

--- components.jsx
+++ components.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    return <h1>Hello JSX</h1>
+    let langs = ["Clojure", "ClojureSript", "JavaScript", "Python"];
+    return(
+      <div>
+        <h1>{"Introduction to ReactJS".toUpperCase()}</h1>
+        <p className="main">Here are some awesome langs:</p>
+        {langs.map(lang => <li>{lang}</li>)}
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('hello'));
+ReactDOM.render(<App/>, document.getElementById('main'));
